4a2fa2936a308529f2461002b42fb44c16e6e742
Tami <tanitati@aol.com>
2022-12-12 19:40:35 +0100

    basics done

---
+++ index.js
@@ -0,0 +1,57 @@
+function getComputerChoice() {
+    randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        return "rock"
+    } else if (randomNumber === 1) {
+        return "paper"
+    } else if (randomNumber === 2) {
+        return "scissors"
+    }
+}
+
+function getUserChoice(userInput) {
+    userInput = userInput.toLowerCase();
+    if (userInput === "rock" || userInput === "paper" || userInput=== "scissors") {
+        return true
+    } else {
+        return "This is not a valid input."
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "This round is a tie!"
+    }
+    if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "You win! Paper beats rock!"
+        } else {
+            return "You lose! Scissors beat paper!"
+        }
+    }
+    if (playerSelection === "rock") {
+        if (computerSelection === "scissors") {
+            return "You win! Rock beats scissors!"
+        } else {
+            return "You lose! Paper beats rock!"
+        }
+    }
+    if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            return "You win! Scissors beat paper!"
+        } else {
+            return "You lose! Rock beats scissors!"
+        }
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound()
+    }
+    console.log(game());
+    console.log(game());
+    console.log(game());
+    console.log(game());
+    console.log(game());
+}
